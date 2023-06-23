465c3f38f1b63094a175a11e2ccd937a6cf6c9bd
heitor augusto <heitoraugusto8@hotmail.com>
2023-06-23 10:14:13 -0300

    feat: add responsive header, nav collapse pending

--- src/Components/MenuButton/index.tsx
+++ src/Components/MenuButton/index.tsx
@@ -1,6 +1,6 @@
 const MenuButton = () => {
   return (
-    <div className='flex gap-2 justify-end container ' id='MenuButton'>
+    <div className='flex gap-2 justify-end container lg:hidden' id='MenuButton'>
       <div>
         <img src='Images/Logo-menuButton.svg' alt='menu-Hamburguer' />
       </div>
--- src/Containers/NavBar/index.tsx
+++ src/Containers/NavBar/index.tsx
@@ -4,29 +4,38 @@ import MenuButton from '../../Components/MenuButton'
 const NavBar = () => {
   return (
     <>
-      <div className='flex flex-row justify-between items-center p-5'>
-        <Logo />
-        <MenuButton />
-      </div>
-      <div className='flex justify-center pt-6 md:justify-end md:pe-12'>
-        <div className='flex flex-col bg-white w-80 drop-shadow-2xl '>
-          <a className='p-6' href='#'>
-            Bende ajyvåsa.
+      <div className='lg:flex container lg:justify-between'>
+        <div className='flex flex-row items-center p-5'>
+          <Logo />
+          <MenuButton />
+        </div>
+        <div className='flex justify-center md:justify-end md:pe-12'>
+          <nav className='flex lg:w-full ps-6 flex-col lg:flex-row w-80 bg-white drop-shadow-2xl lg:drop-shadow-none lg:items-center'>
+            <ul className='lg:flex lg:flex-row '>
+              <li className='p-6'>
+                <a href='#'>Bende ajyvåsa.</a>
+              </li>
+              <li className='p-6'>
+                <a href='#'>Sutesm asonat.</a>
+              </li>
+              <li className='p-6'>
+                <a href='#'>Ding pararat.</a>
+              </li>
+              <li className='p-6'>
+                <a href='#'>Teleska.</a>
+              </li>
+            </ul>
+            <div className='p-6 lg:hidden'>
+              <a className=' bg-sky-600 pt-4 pb-4 ps-6 pe-6 rounded-full text-white' href='#'>
+                Try it Now
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className='p-6 lg:flex lg:flex-row lg:ml-auto hidden'>
+          <a className=' bg-sky-600 pt-4 pb-4 ps-6 pe-6 rounded-full text-white' href='#'>
+            Try it Now
           </a>
-          <a className='p-6' href='#'>
-            Sutesm asonat.
-          </a>
-          <a className='p-6' href='#'>
-            Ding pararat.
-          </a>
-          <a className='p-6' href='#'>
-            Teleska.
-          </a>
-          <div className='p-6'>
-            <a className=' bg-sky-600 pt-4 pb-4 ps-6 pe-6 rounded-full text-white' href='#'>
-              Try it Now
-            </a>
-          </div>
         </div>
       </div>
     </>
