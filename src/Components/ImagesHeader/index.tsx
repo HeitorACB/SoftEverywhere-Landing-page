071b57fdad4f25d5bbaf57ac8c4c2d35c5a706b8
heitor augusto <heitoraugusto8@hotmail.com>
2023-06-26 11:36:17 -0300

    fix: adjustment os images, adding a max-width(1200px) to not exceed the container

--- src/Components/ImagesHeader/index.tsx
+++ src/Components/ImagesHeader/index.tsx
@@ -1,6 +1,6 @@
 const ImageHeader = () => {
   return (
-    <div className='flex justify-center md:justify-end md:pe-4'>
+    <div className='flex justify-center md:justify-end md:pe-4 max-w-[1200px] mx-auto'>
       <div className='flex w-96 fixed'>
         <img className='relative' src='Images/IMAGE-BACK-HEADER.png' alt='' />
 
