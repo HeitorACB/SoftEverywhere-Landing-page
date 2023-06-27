16b7a710cc42426c019a7d313b3c6eb8ae41a255
heitor augusto <heitoraugusto8@hotmail.com>
2023-06-27 10:45:38 -0300

    fix: changing input size

--- src/Components/InputHeader/index.tsx
+++ src/Components/InputHeader/index.tsx
@@ -1,15 +1,15 @@
 const InputHeader = () => {
   return (
-    <div className='ps-5 mt-4 mb-96 flex items-center'>
+    <div className='max-w-[1200px] mx-auto ps-4 mt-4 mb-96 flex items-center'>
       <div>
         <input
-          className='bg-graylight p-5 pe-0 rounded-full lg:pe-48'
+          className='bg-graylight p-4 pe-0 rounded-full lg:pe-48'
           type='text'
           placeholder='Infrara favis.'
         />
       </div>
       <div className='bg-Bluelight rounded-full ms-1 lg:hidden'>
-        <img className='p-5 ' src='Images/arrow-right.svg' alt='' />
+        <img className='p-4 ' src='Images/arrow-right.svg' alt='' />
       </div>
       <div className='hidden lg:flex bg-Bluelight  rounded-full ms-4'>
         <button className='text-BluePrimary font-medium leading-5 p-6'>Lets Talk</button>
