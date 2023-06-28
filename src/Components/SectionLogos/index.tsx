2067d310fe5052724e72187365b6ff48fce242f8
heitor augusto <heitoraugusto8@hotmail.com>
2023-06-28 10:58:11 -0300

    feat(animation): add hover animation to images

--- src/Components/SectionLogos/index.tsx
+++ src/Components/SectionLogos/index.tsx
@@ -2,11 +2,32 @@ const Logos = () => {
   return (
     <>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-16 justify-items-center md:justify-items-between lg:max-w-[1200px] mx-auto md:pb-16'>
-        <img className='' src='Images/SectionLogos/Spotify.svg' alt='' />
-        <img className='' src='Images/SectionLogos/Uber.svg' alt='' />
-        <img className='' src='Images/SectionLogos/Google.svg' alt='' />
-        <img className='' src='Images/SectionLogos/Shopify.svg' alt='' />
-        <img className='mb-16 md:mb-0' src='Images/SectionLogos/Microsoft.svg' alt='' />
+        <img
+          className='filter hover:filter-none grayscale hover:grayscale-0 transition-all duration-500 ease-in-out'
+          src='Images/SectionLogos/Spotify.svg'
+          alt=''
+        />
+        <img
+          className='filter hover:brightness-0 transition-all duration-500 ease-in-out'
+          src='Images/SectionLogos/Uber_Gray.svg'
+          alt=''
+        />
+
+        <img
+          className='filter hover:filter-none grayscale hover:grayscale-0 transition-all duration-500 ease-in-out'
+          src='Images/SectionLogos/Google.svg'
+          alt=''
+        />
+        <img
+          className='filter hover:filter-none grayscale hover:grayscale-0 transition-all duration-500 ease-in-out'
+          src='Images/SectionLogos/Shopify.svg'
+          alt=''
+        />
+        <img
+          className='mb-16 md:mb-0 filter hover:filter-none grayscale hover:grayscale-0 transition-all duration-500 ease-in-out'
+          src='Images/SectionLogos/Microsoft.svg'
+          alt=''
+        />
       </div>
     </>
   )
